6ec0f7503677bf6feb5c8f75c4a225f1d1249336
lee1066515 <lee1066515@naver.com>
2023-01-12 18:42:07 +0900

    2023-01-12 18:40 이장한 로그인 엑티비티에서 구현이 되지 않았던 아이디 입력시 버튼 활성화 구현 및 뒤로가기 버튼 2번 입력시 종료 등의 기능을 추가적으로 구현하였습니다.

--- app/src/main/java/com/example/umc_3rd_application/MainActivity.kt
+++ app/src/main/java/com/example/umc_3rd_application/MainActivity.kt
@@ -1,11 +1,29 @@
 package com.example.umc_3rd_application
 
+
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+
 
 class MainActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_loginactivity)
+        setContentView(R.layout.activity_main)
+
+
+
+        //2022-01-12
+        //현재는 mainactivity에서 할 일이 없기 때문에, hello world 버튼을 누르면 loginactivity로 이동하는 코드만 작성하도록 하겠습니다.
+        val mainButton = findViewById<Button>(R.id.mainActivityButton)
+        mainButton.setOnClickListener {
+
+            val intent = Intent(this, loginactivity::class.java)
+            startActivity(intent)
+        }
+
     }
 }
