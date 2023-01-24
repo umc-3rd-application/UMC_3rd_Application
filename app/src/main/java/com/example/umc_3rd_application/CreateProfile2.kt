c6e9dbd59dc0e79c3eab5a5f79d67d58cea1398a
jeonghoon <fanatel@naver.com>
2023-01-24 22:32:56 +0900

    2023-01-24 22:32 김정훈 CreateProfile2 activity 추가

---
+++ app/src/main/java/com/example/umc_3rd_application/CreateProfile2.kt
@@ -0,0 +1,11 @@
+package com.example.umc_3rd_application
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CreateProfile2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_profile2)
+    }
+}
