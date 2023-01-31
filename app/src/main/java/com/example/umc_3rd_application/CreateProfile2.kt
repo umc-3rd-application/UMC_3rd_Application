ac5952a5d441ee1d6591b3433ad80e4edaadf9bd
Jeon, Kyunghwan <114416829+JJ-HMFIC@users.noreply.github.com>
2023-02-01 01:49:09 +0900

    전경환
    연동 수정

--- app/src/main/java/com/example/umc_3rd_application/CreateProfile2.kt
+++ app/src/main/java/com/example/umc_3rd_application/CreateProfile2.kt
@@ -5,7 +5,7 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.umc_3rd_application.databinding.ActivityCreateProfile2Binding
 
-class CreateProfile2Activity:AppCompatActivity() {
+class CreateProfile2: AppCompatActivity() {
     private lateinit var viewBinding : ActivityCreateProfile2Binding
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,7 +21,7 @@ class CreateProfile2Activity:AppCompatActivity() {
             add(Profile2Data("7","8","9"))
             add(Profile2Data("10","11","12"))
         }
-        val profile2RVAdapter = Profile2RVAdapter(dataList)
+        val profile2RVAdapter = ProfileRVAdapter(dataList)
         // 이 다음 뭐써야되냐 .. layoutmanager 호출 해야되는데
 
 
